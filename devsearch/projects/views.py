9a2046d2ab25b45ce9c061f30e2552aaf7e795bb
Archit Maheshwari <arcmahes@cisco.com>
2021-11-15 13:01:35 +0530

    jinja templating

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,10 +1,24 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+projectList = [
+    {
+        'id':'1',
+        'name': 'archit'
+    },
+    {
+        'id':'2',
+        'name': 'casper'
+    }
+]
 def projects(request):
-    return render(request, 'projects/projects.html')
+    context ={"projects":projectList}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None 
+    for i in projectList:
+        if i["id"] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})
