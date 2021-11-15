fe6313917c984844b287df307fd766b0e5cdf864
Archit Maheshwari <arcmahes@cisco.com>
2021-11-15 20:05:38 +0530

    CRUD functionality added

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import project, projects, createProject
+from .views import project, projects, createProject, updateProject, deleteProject
 
 urlpatterns = [
     path('', projects, name="projects"),
     path('project/<str:pk>', project, name="project"),
     path('createProject/', createProject, name="create-project"),
+    path('updateProject/<str:pk>', updateProject, name="update-project"),
+    path('deleteProject/<str:pk>', deleteProject, name="delete-project"),
 ]
--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Project
 from .forms import ProjectForm
 # Create your views here.
@@ -17,4 +17,27 @@ def project(request, pk):
 def createProject(request):
     form = ProjectForm()
     context={'form':form}
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
     return render(request, 'projects/project_form.html', context)
+
+def updateProject(request, pk):
+    form = ProjectForm(instance=Project.objects.get(id=pk))
+    context={'form':form}
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=Project.objects.get(id=pk))
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    return render(request, 'projects/project_form.html', context)
+
+def deleteProject(request, pk):
+    project=Project.objects.get(id=pk)
+    context={'project':project}
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    return render(request, 'projects/delete-confirm.html', context)
