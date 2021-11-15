ea63457dfb2f14eb87525737d9c0ab3482fc3e65
Archit Maheshwari <arcmahes@cisco.com>
2021-11-15 14:18:43 +0530

    project model added

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=200, null=True, blank=True)
+    source_link = models.CharField(max_length=200, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+
+    def __str__(self):
+        return self.title
