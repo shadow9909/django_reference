60b99a56f2c41620fc3b8e7409ff0776f671ef8a
Archit Maheshwari <arcmahes@cisco.com>
2021-11-15 12:04:36 +0530

    adding template folder

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def projects(request):
-    return HttpResponse('projects')
+    return render(request, 'projects.html')
 
 def project(request, pk):
-    return HttpResponse('project'+pk)
+    return render(request, 'single-project.html')
