2c2a67273b75c206512bdd8cb50c43f8f462d221
Archit Maheshwari <arcmahes@cisco.com>
2021-11-15 14:42:00 +0530

    Added tag(many to many) and review(one to many) Model

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -8,6 +8,32 @@ class Project(models.Model):
     source_link = models.CharField(max_length=200, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    tag = models.ManyToManyField('Tag')
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up','UP VOTE'),
+        ('down','DOWN VOTE')
+    )
+    # owner = 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(choices=VOTE_TYPE, max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+
+    def __str__(self):
+        return self.name
