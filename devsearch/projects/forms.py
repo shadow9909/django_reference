b4523152704c667a1af2b3dacc2ab0b9559d6295
Archit Maheshwari <arcmahes@cisco.com>
2021-11-16 08:52:59 +0530

    adding uikit

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,7 +1,18 @@
-from django.forms import ModelForm
-from .models import Project
+from django.db.models.base import Model
+from django.forms import ModelForm, widgets
+from django import forms
+from .models import Project, Review
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title','description', 'demo_link','source_link','tag', 'featured_image']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
