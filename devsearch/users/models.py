07c29e7acc06a3c93789cf61e39d7931de81bb53
Archit Maheshwari <arcmahes@cisco.com>
2021-11-18 12:08:42 +0530

    Message model created

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -6,33 +6,61 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 # Create your models here.
+
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     username = models.CharField(max_length=200, blank=True, null=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=200, blank=True, null=True)
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
-    profile_image= models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/logo.jpg')
+    profile_image = models.ImageField(
+        null=True, blank=True, upload_to='profiles/', default='profiles/logo.jpg')
     social_github = models.CharField(max_length=200, blank=True, null=True)
     social_twitter = models.CharField(max_length=200, blank=True, null=True)
-    
+
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4,
+                          primary_key=True, unique=True, editable=False)
 
     def __str__(self):
         return str(self.user.username)
 
+
 class Skills(models.Model):
-    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    owner = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4,
+                          primary_key=True, unique=True, editable=False)
     name = models.CharField(max_length=200, blank=True, null=True)
-    bio = models.CharField(max_length=200,blank=True, null=True)
+    bio = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return str(self.name)
 
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, null=True, blank=True)
+    recipient = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, null=True, related_name="messages")
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    subject = models.CharField(max_length=200, blank=True, null=True)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True,
+                          unique=True, editable=False)
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        ordering = ['is_read', '-created']
 
 # shifted to signals.py
 
@@ -47,7 +75,7 @@ class Skills(models.Model):
 #             name=user.first_name
 #         )
 
-# #when profile is deleted the user should also be deleted    
+# #when profile is deleted the user should also be deleted
 # @receiver(post_delete, sender=Profile)
 # def deleteUser(sender, instance, **kwargs):
 #     user = instance.user
